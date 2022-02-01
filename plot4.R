b45bcf4b70348b64671cb6cdc83f0af5bb0b58ca
practicingR <97630782+practicingR@users.noreply.github.com>
2022-02-01 12:37:49 -0500

    Set columns as numeric

--- plot4.R
+++ plot4.R
@@ -18,3 +18,10 @@
 
      ### Subset data for dates 1/2/2007 and 2/2/2007 (two-day period)
           powerconsumptionsubset <- subset(householdpowerconsumption, householdpowerconsumption$Date =="1/2/2007" | householdpowerconsumption$Date=="2/2/2007")
+
+## Set variables as a numeric class
+     powerconsumptionsubset$Global_active_power <- as.numeric(powerconsumptionsubset$Global_active_power)
+     powerconsumptionsubset$Sub_metering_1 <- as.numeric(powerconsumptionsubset$Sub_metering_1)
+     powerconsumptionsubset$Sub_metering_2 <- as.numeric(powerconsumptionsubset$Sub_metering_2)
+     powerconsumptionsubset$Sub_metering_3 <- as.numeric(powerconsumptionsubset$Sub_metering_3)
+     powerconsumptionsubset$Global_reactive_power <- as.numeric(powerconsumptionsubset$Global_reactive_power)
