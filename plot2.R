da884f44cb9fd002da3387231b9933ca1e93bcfd
practicingR <97630782+practicingR@users.noreply.github.com>
2022-02-01 12:25:05 -0500

    Set Global_active_power as numeric

--- plot2.R
+++ plot2.R
@@ -18,3 +18,6 @@
 
      ### Subset data for dates 1/2/2007 and 2/2/2007 (two-day period)
           powerconsumptionsubset <- subset(householdpowerconsumption, householdpowerconsumption$Date =="1/2/2007" | householdpowerconsumption$Date=="2/2/2007")
+
+## Set Global_active_power as a numeric class
+     powerconsumptionsubset$Global_active_power <- as.numeric(powerconsumptionsubset$Global_active_power)
